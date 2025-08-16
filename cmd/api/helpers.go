e058cb0fd5ffd4f853e2ad01177a68204545eebb
atharva <atharva.prabhune2021@gmail.com>
2025-08-16 21:46:13 +0530

    Improved error response for wrong input

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
 	"maps"
 	"net/http"
 	"strconv"
@@ -12,6 +14,10 @@ import (
 
 type envelope map[string]any
 
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+}
+
 func (app *application) readIDParam(r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
 	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
@@ -19,6 +25,33 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 		return 0, errors.New("invalid id parameter")
 	}
 	return id, nil
+}
+
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any) error {
+	err := json.NewDecoder(r.Body).Decode(dst)
+	if err != nil {
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+		var invalidUnmarshalError *json.InvalidUnmarshalError
+		switch {
+			case errors.As(err, &syntaxError):
+				return fmt.Errorf("body contains badly formed JSON (at character %d)", syntaxError.Offset)
+			case errors.Is(err, io.ErrUnexpectedEOF):
+				return errors.New("body contains badly formed JSON ")
+			case errors.As(err, &unmarshalTypeError):
+				if unmarshalTypeError.Field != "" {
+					return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
+				}
+				return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
+			case errors.Is(err, io.EOF):
+				return errors.New("Body must not be empty")
+			case errors.As(err, &invalidUnmarshalError):
+				panic(err)
+			default:
+				return err
+		}
+	}
+	return nil
 }
 
 func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
