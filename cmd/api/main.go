206214b55002b530bd0c43473f375de15640f438
atharva <atharva.prabhune2021@gmail.com>
2025-08-20 21:15:28 +0530

    Added basic models code for database integration

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,6 +10,8 @@ import (
 	"os"
 	"time"
 
+	"movienode.atharva.net/internal/data"
+
 	_ "github.com/lib/pq"
 ) 
 
@@ -29,6 +31,7 @@ type config struct {
 type application struct {
 	config config
 	logger *log.Logger
+	models data.Models
 }
 
 func main() {
@@ -50,6 +53,7 @@ func main() {
 	app := &application {
 		config: cfg,
 		logger: logger,
+		models: data.NewModels(db),
 	}
 	srv := &http.Server{
 		Addr: fmt.Sprintf(":%d", cfg.port),
