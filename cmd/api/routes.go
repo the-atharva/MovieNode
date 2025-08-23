f70d31344ddfb59a602372d30e0eac35700afd8f
atharva <atharva.prabhune2021@gmail.com>
2025-08-23 14:40:15 +0530

    Added PUT endpoint feature

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -11,5 +11,6 @@ func (app *application) routes() *httprouter.Router {
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
+	router.HandlerFunc(http.MethodPut, "/v1/movies/:id", app.updateMovieHandler)
 	return router
 }
