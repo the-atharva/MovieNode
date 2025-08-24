37629630a37559a0703430c0ef39dbd3f23b927d
atharva <atharva.prabhune2021@gmail.com>
2025-08-24 10:55:50 +0530

    Changed formatting

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -47,7 +47,11 @@ func (m MovieModel) Delete(id int64) error {
 
 
 func (m MovieModel) Insert(movie *Movie) error {
-	query := `INSERT INTO movies (title, year, runtime, genres) VALUES ($1, $2, $3, $4) RETURNING id, created_at, version`
+	query := 
+			`INSERT INTO movies 
+			(title, year, runtime, genres) 
+			VALUES ($1, $2, $3, $4) 
+			RETURNING id, created_at, version`
 	args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
 	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
 }
@@ -56,9 +60,11 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 	if id < 1 {
 		return nil, ErrRecordNotFound
 	}
-	query := `SELECT id, created_at, title, year, runtime, genres, version
-				FROM movies
-				WHERE id = $1`
+	query := 
+			`SELECT 
+			id, created_at, title, year, runtime, genres, version
+			FROM movies
+			WHERE id = $1`
 	var movie Movie
 	err := m.DB.QueryRow(query, id).Scan(
 			&movie.ID,
