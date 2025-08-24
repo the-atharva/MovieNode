22ef1df26af78f90bb6f0aafb8d3bdd737db3589
atharva <atharva.prabhune2021@gmail.com>
2025-08-24 17:31:20 +0530

    Added context timeout for all database queries

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"context"
 	"database/sql"
 	"errors"
 	"time"
@@ -31,7 +32,9 @@ func (m MovieModel) Delete(id int64) error {
 	query := 	
 			`DELETE FROM movies
 			WHERE id = $1`
-	result, err := m.DB.Exec(query, id)
+	ctx, cancel := context.WithTimeout(context.Background(), 3 * time.Second)
+	defer cancel()
+	result, err := m.DB.ExecContext(ctx, query, id)
 	if err != nil {
 		return err
 	}
@@ -53,7 +56,9 @@ func (m MovieModel) Insert(movie *Movie) error {
 			VALUES ($1, $2, $3, $4) 
 			RETURNING id, created_at, version`
 	args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
-	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
+	ctx, cancel := context.WithTimeout(context.Background(), 3 * time.Second)
+	defer cancel()
+	return m.DB.QueryRowContext(ctx, query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
 }
 
 func (m MovieModel) Get(id int64) (*Movie, error) {
@@ -66,7 +71,9 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 			FROM movies
 			WHERE id = $1`
 	var movie Movie
-	err := m.DB.QueryRow(query, id).Scan(
+	ctx, cancel := context.WithTimeout(context.Background(), 3 * time.Second)
+	defer cancel()
+	err := m.DB.QueryRowContext(ctx, query, id).Scan(
 			&movie.ID,
 			&movie.CreatedAt,
 			&movie.Title,
@@ -90,7 +97,7 @@ func (m MovieModel) Update(movie *Movie) error {
 	query := 
 			`UPDATE movies
 			SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
-			WHERE id = $5
+			WHERE id = $5 AND version = $6
 			RETURNING version`
 	args := []any{
 			movie.Title,
@@ -98,8 +105,20 @@ func (m MovieModel) Update(movie *Movie) error {
 			movie.Runtime,
 			pq.Array(movie.Genres),
 			movie.ID,
+			movie.Version,
 	}
-	return m.DB.QueryRow(query, args...).Scan(&movie.Version)	
+	ctx, cancel := context.WithTimeout(context.Background(), 3 * time.Second)
+	defer cancel()
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(&movie.Version)	
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrEditConflict
+		default:
+			return err
+		}
+	}
+	return nil
 }
 
 func ValidateMovie(v *validator.Validator, movie *Movie) {
