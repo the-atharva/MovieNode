49733eb719a51dad249004bb3f0c266fe3a68180
atharva <atharva.prabhune2021@gmail.com>
2025-08-31 23:49:40 +0530

    Added feature for sorting movies

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,6 +1,9 @@
 package data
 
-import(
+import (
+	"slices"
+	"strings"
+
 	"movienode.atharva.net/internal/validator"
 )
 
@@ -9,6 +12,25 @@ type Filters struct {
 	PageSize int
 	Sort string
 	SortSafeList []string
+}
+
+func (f Filters) sortColumn() string {
+	// for _, safeValue := range f.SortSafeList {
+	// 	if f.Sort == safeValue {
+	// 		return strings.TrimPrefix(f.Sort, "-")
+	// 	}
+	// }
+	if slices.Contains(f.SortSafeList, f.Sort) {
+		return strings.TrimPrefix(f.Sort, "-")
+	}
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+	return "ASC"
 }
 
 func ValidateFilters(v *validator.Validator, f Filters) {
