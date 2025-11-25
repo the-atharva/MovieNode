adf2fd4645a4d807f2bc3ae542a9a19281278ebd
atharva <atharva.prabhune2021@gmail.com>
2025-11-25 21:39:42 +0530

    Added permission based access for users

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -61,6 +61,11 @@ func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request)
 	app.errorResponse(w, r, http.StatusNotFound, message)
 }
 
+func (app *application) notPermittedResponse(w http.ResponseWriter, r *http.Request) {
+	message := "your user account doesn't have the necessary permissions to access this resource"
+	app.errorResponse(w, r, http.StatusForbidden, message)
+}
+
 func (app *application) rateLimitExceededResponse(w http.ResponseWriter, r *http.Request) {
 	message := "rate limit exceeded"
 	app.errorResponse(w, r, http.StatusTooManyRequests, message)
