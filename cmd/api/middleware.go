49121fabdd104945058f06f0a40a9a485fd34cdc
atharva <atharva.prabhune2021@gmail.com>
2025-12-04 19:02:59 +0530

    Added metrics endpoint & middlewares

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"errors"
+	"expvar"
 	"fmt"
 	"net"
 	"net/http"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
 
+	"github.com/felixge/httpsnoop"
 	"golang.org/x/time/rate"
 	"movienode.atharva.net/internal/data"
 	"movienode.atharva.net/internal/validator"
@@ -68,6 +71,21 @@ func (app *application) enableCORS(next http.Handler) http.Handler {
 			}
 		}
 		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) metrics(next http.Handler) http.Handler {
+	totalRequestsReceived := expvar.NewInt("total_requests_received")
+	totalResponsesSent := expvar.NewInt("total_responses_sent")
+	totalProcessingTimeMicroseconds := expvar.NewInt("total_processing_time_us")
+	totalResponseesSentByStatus := expvar.NewMap("total_responses_sent_by_status")
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		totalRequestsReceived.Add(1)
+		metrics := httpsnoop.CaptureMetrics(next, w, r)
+		totalResponsesSent.Add(1)
+		totalProcessingTimeMicroseconds.Add(metrics.Duration.Microseconds())
+		totalResponseesSentByStatus.Add(strconv.Itoa(metrics.Code), 1)
+
 	})
 }
 
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24.0
 require github.com/julienschmidt/httprouter v1.3.0
 
 require (
+	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/go-mail/mail/v2 v2.3.0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	golang.org/x/crypto v0.43.0 // indirect
