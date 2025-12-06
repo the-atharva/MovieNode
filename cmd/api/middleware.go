48563ab461c467325017d785f9d6354e1e0ec39e
atharva <atharva.prabhune2021@gmail.com>
2025-12-06 16:57:48 +0530

    Modified rate limiter to be compatible with reverse proxy

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"expvar"
 	"fmt"
-	"net"
 	"net/http"
 	"strconv"
 	"strings"
@@ -12,6 +11,7 @@ import (
 	"time"
 
 	"github.com/felixge/httpsnoop"
+	"github.com/tomasen/realip"
 	"golang.org/x/time/rate"
 	"movienode.atharva.net/internal/data"
 	"movienode.atharva.net/internal/validator"
@@ -112,11 +112,7 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 	}()
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if app.config.limiter.enabled {
-			ip, _, err := net.SplitHostPort(r.RemoteAddr)
-			if err != nil {
-				app.serverErrorResponse(w, r, err)
-				return
-			}
+			ip := realip.FromRequest(r)
 			mu.Lock()
 			if _, found := clients[ip]; !found {
 				clients[ip] = &client{
